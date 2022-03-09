083e3e002a2c2b437d1c2458cefb7d0c969e846a
Casper Riis <Casper.riis.lie.nielsen@gmail.com>
2022-03-09 09:38:58 +0100

    Add project files.

---
+++ Car Class Library/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Car_Class_Library
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        private string _model { get; set; }
+        private int _price { get; set; }
+        private string _license { get; set; }
+
+        private const int ModelMinLength = 4;
+        private const int PriceMin = 0;
+        private const int LicenseMin = 2;
+        private const int LicenseMax = 7;
+
+        public Car(int id, string model, int price, string license)
+        {
+            Id = id;
+            Model = model;
+            Price = price;
+            License = license;
+        }
+
+        public Car()
+        {
+            
+        }
+
+        /// <summary>
+        /// Model of a car
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">if model string length is not [4;inf[</exception>
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(Model), "Model is null");
+                if (value.Length < ModelMinLength) throw new ArgumentOutOfRangeException(nameof(Model), $"Model: '{value}' length must be (>{ModelMinLength})");
+                _model = value;
+            }
+        }
+
+        /// <summary>
+        /// Price of a car
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">if price value is not ]0;inf[</exception>
+        public int Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= PriceMin) throw new ArgumentOutOfRangeException(nameof(Price), $"Price: {value} out of range (>{PriceMin})");
+                _price = value;
+            }
+        }
+
+        /// <summary>
+        /// License plate of a car
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">if license string length is not [2;7]</exception>
+        public string License
+        {
+            get => _license;
+            set
+            {
+                if (value == null) throw new ArgumentNullException(nameof(License), "License is null");
+                if (value.Length is < LicenseMin or > LicenseMax) throw new ArgumentOutOfRangeException(nameof(License), $"License: '{value}' length must be [{LicenseMin};{LicenseMax}]");
+                _license = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} - Model: {Model} - Price: {Price} - License: {License}";
+        }
+    }
+}
---
+++ Car Class LibraryTests/CarTests.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Car_Class_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Car_Class_Library.Car;
+
+namespace Car_Class_Library.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car _car;
+        [TestInitialize]
+        public void SetUp()
+        {
+            _car = new Car(1, "F150", 20000, "123ABC4");
+        }
+
+        [TestMethod()]
+        public void TestCarConstructor()
+        {
+            Car car2 = new Car(2, "F200", 30000, "ABC123D");
+            Assert.AreEqual(2, car2.Id);
+            Assert.AreEqual("F200", car2.Model);
+            Assert.AreEqual(30000, car2.Price);
+            Assert.AreEqual("ABC123D", car2.License);
+        }
+        [TestMethod()]
+        public void TestCarModel()
+        {
+            _car.Model = "F150";
+            Assert.AreEqual("F150", _car.Model);
+            Assert.ThrowsException<ArgumentNullException>(() => _car.Model = null);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Model = "F15");
+        }
+
+        [TestMethod()]
+        public void TestCarPrice()
+        {
+            _car.Price = 20000;
+            Assert.AreEqual(20000, _car.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Price = 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.Price = -1);
+        }
+        [TestMethod()]
+        public void TestCarLicense()
+        {
+            _car.License = "123ABC4";
+            Assert.AreEqual("123ABC4", _car.License);
+            Assert.ThrowsException<ArgumentNullException>(() => _car.License = null);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.License = "123ABC45"); 
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _car.License = "1"); 
+        }
+
+    }
+}
